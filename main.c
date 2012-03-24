8eaf82f0d6348238d39b7a590650efbf6be3e3a6
David Sveningsson <ext@sidvind.com>
2012-03-24 14:48:09 +0100

    Reindenting code. During the time this code was written my editor had been misconfigured to used spaces instead of tabs.

--- main.c
+++ main.c
@@ -23,22 +23,22 @@
 #define DEFAULT_SOCK_PATH "./parserver.sock"
 
 static struct option long_options[] = {
-  {"port", required_argument, 0, 'p'},
-  {"listen", required_argument, 0, 'l'},
-  {"help", no_argument, 0, 'h'},
-  {0,0,0,0} /* sentinel */
+	{"port", required_argument, 0, 'p'},
+	{"listen", required_argument, 0, 'l'},
+	{"help", no_argument, 0, 'h'},
+	{0,0,0,0} /* sentinel */
 };
 
 void show_usage(void){
-  printf("(C) 2011 David Sveningsson <ext@sidvind.com>\n");
-  printf("usage: parserver [OPTIONS] DEVICE\n");
-  printf("  -p, --port=PORT    Listen on port [default: %d]\n"
-	 "  -l, --listen=IP    Listen on on ip [default: 127.0.0.1]\n"
-	 "  -h, --help         This text.\n"
-	 "\n"
-	 "If neither -l or -p is given it listens on unix domain socket.\n"
-	 "Device is usually /dev/parport0\n"
-	 , DEFAULT_LISTEN_PORT);
+	printf("(C) 2011 David Sveningsson <ext@sidvind.com>\n");
+	printf("usage: parserver [OPTIONS] DEVICE\n");
+	printf("  -p, --port=PORT    Listen on port [default: %d]\n"
+	       "  -l, --listen=IP    Listen on on ip [default: 127.0.0.1]\n"
+	       "  -h, --help         This text.\n"
+	       "\n"
+	       "If neither -l or -p is given it listens on unix domain socket.\n"
+	       "Device is usually /dev/parport0\n"
+	       , DEFAULT_LISTEN_PORT);
 }
 
 static int running = 1;
@@ -46,229 +46,229 @@ static int port = DEFAULT_LISTEN_PORT;
 static in_addr_t ip;
 
 void sigint_handler(int sig){
-  running = 0;
-  putc('\r', stderr);
+	running = 0;
+	putc('\r', stderr);
 }
 
 static int port_open(const char* port){
-  // Open the parallel port for reading and writing
-  int fd = open(port, O_RDWR);
-  
-  if ( fd == -1 ){
-    perror("Failed to open parallel port");
-    return -1;
-  }
+	// Open the parallel port for reading and writing
+	int fd = open(port, O_RDWR);
 
-  // Try to claim port
-  if ( ioctl(fd, PPCLAIM, NULL) ){
-    perror("Could not claim parallel port");
-    close(fd);
-    return -1;
-  }
+	if ( fd == -1 ){
+		perror("Failed to open parallel port");
+		return -1;
+	}
 
-  // Set the Mode
-  static const int mode = IEEE1284_MODE_BYTE;
-  if ( ioctl(fd, PPSETMODE, &mode) ){
-    perror("Could not set mode");
-    ioctl(fd, PPRELEASE);
-    close(fd);
-    return -1;
-  }
+	// Try to claim port
+	if ( ioctl(fd, PPCLAIM, NULL) ){
+		perror("Could not claim parallel port");
+		close(fd);
+		return -1;
+	}
 
-  // Set data pins to output
-  static const int dir = 0x00;
-  if ( ioctl(fd, PPDATADIR, &dir) ){
-    perror("Could not set parallel port direction");
-    ioctl(fd, PPRELEASE);
-    close(fd);
-    return -1;
-  }
+	// Set the Mode
+	static const int mode = IEEE1284_MODE_BYTE;
+	if ( ioctl(fd, PPSETMODE, &mode) ){
+		perror("Could not set mode");
+		ioctl(fd, PPRELEASE);
+		close(fd);
+		return -1;
+	}
 
-  return fd;
+	// Set data pins to output
+	static const int dir = 0x00;
+	if ( ioctl(fd, PPDATADIR, &dir) ){
+		perror("Could not set parallel port direction");
+		ioctl(fd, PPRELEASE);
+		close(fd);
+		return -1;
+	}
+
+	return fd;
 }
 
 int main(int argc, char* argv[]){
-  ip = inet_addr("127.0.0.1");
-  const char* sock_path = DEFAULT_SOCK_PATH;
-  int exit_code = 1;
+	ip = inet_addr("127.0.0.1");
+	const char* sock_path = DEFAULT_SOCK_PATH;
+	int exit_code = 1;
 
-  int option_index = 0;
-  int op;
+	int option_index = 0;
+	int op;
 
-  while ( (op = getopt_long(argc, argv, "", long_options, &option_index)) != -1 )
-    switch (op){
-    case 0: /* longopt with flag */
-    case '?': /* unknown */
-      break;
-      
-    case 'p': /* --port */
-      port = atoi(optarg);
-      break;
+	while ( (op = getopt_long(argc, argv, "", long_options, &option_index)) != -1 )
+		switch (op){
+		case 0: /* longopt with flag */
+		case '?': /* unknown */
+			break;
 
-    case 'l': /* --listen */
-      ip = inet_addr(optarg);
-      break;
+		case 'p': /* --port */
+			port = atoi(optarg);
+			break;
 
-    case 'h': /* --help */
-      show_usage();
-      exit(0);
+		case 'l': /* --listen */
+			ip = inet_addr(optarg);
+			break;
 
-    default:
-      fprintf(stderr, "declared but unhandled argument -%c\n", op);
-      break;
-    }
+		case 'h': /* --help */
+			show_usage();
+			exit(0);
 
-  if ( optind == argc ){
-    fprintf(stderr, "Missing device, see --help for usage.\n");
-    exit(1);
-  }
+		default:
+			fprintf(stderr, "declared but unhandled argument -%c\n", op);
+			break;
+		}
 
-  const char* device = argv[optind];
-   
-  fprintf(stderr, "Parallel port interface server\n");
+	if ( optind == argc ){
+		fprintf(stderr, "Missing device, see --help for usage.\n");
+		exit(1);
+	}
 
-  /* handle signals */
-  signal(SIGINT, sigint_handler);
+	const char* device = argv[optind];
 
-  /* open listening socket */
-  struct sockaddr_un addr = {0,};
-  int sock = socket(AF_UNIX, SOCK_STREAM, 0);
-  if ( sock == -1 ){
-    perror("Failed to create socket");
-    return 1;
-  }
-  int retry = 0;
-  while ( retry++ < 3 ){ /* retry at most 2 times */
-	  addr.sun_family = AF_UNIX;
-	  snprintf(addr.sun_path, sizeof(addr.sun_path), "%s", sock_path);
-	  if ( bind(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1 ){
-		  if ( errno == EADDRINUSE ){ /* adress already used, try to connect to the socket */
-			  if ( connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == 0 ){
-				  fprintf(stderr, "daemon already running\n");
-				  exit(0);
-			  } else {
-				  /* could not connect, assume it is dead */
-				  fprintf(stderr, "dead daemon deteched, removing socket\n");
-				  unlink(addr.sun_path);
-				  continue;
-			  }
-		  } else {
-			  perror("failed to bind socket");
-			  return 1;
-		  }
-	  }
-	  break;
-  }
-  if ( retry == 3 ){ /* failed to bind */
-	  fprintf(stderr, "failed to bind socket\n");
-	  exit(1);
-  }
-  if ( listen(sock, 1) == -1){
-    perror("failed to listen");
-  }
+	fprintf(stderr, "Parallel port interface server\n");
 
-  /* fix permissions on socket */
-  chmod(addr.sun_path, 0666);
+	/* handle signals */
+	signal(SIGINT, sigint_handler);
 
-  int fd;
-  fprintf(stderr, "Opening device %s\n", device);
-  if ( (fd=port_open(device)) == -1 ){
-    goto socket_cleanup; /* error already shown */
-  }
+	/* open listening socket */
+	struct sockaddr_un addr = {0,};
+	int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if ( sock == -1 ){
+		perror("Failed to create socket");
+		return 1;
+	}
+	int retry = 0;
+	while ( retry++ < 3 ){ /* retry at most 2 times */
+		addr.sun_family = AF_UNIX;
+		snprintf(addr.sun_path, sizeof(addr.sun_path), "%s", sock_path);
+		if ( bind(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1 ){
+			if ( errno == EADDRINUSE ){ /* adress already used, try to connect to the socket */
+				if ( connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == 0 ){
+					fprintf(stderr, "daemon already running\n");
+					exit(0);
+				} else {
+					/* could not connect, assume it is dead */
+					fprintf(stderr, "dead daemon deteched, removing socket\n");
+					unlink(addr.sun_path);
+					continue;
+				}
+			} else {
+				perror("failed to bind socket");
+				return 1;
+			}
+		}
+		break;
+	}
+	if ( retry == 3 ){ /* failed to bind */
+		fprintf(stderr, "failed to bind socket\n");
+		exit(1);
+	}
+	if ( listen(sock, 1) == -1){
+		perror("failed to listen");
+	}
 
-  /* all pins low */
-  char dataH = 0x00;
-  char dataL = 0x00;
+	/* fix permissions on socket */
+	chmod(addr.sun_path, 0666);
 
-  while ( running ){
-    printf("current output: 0x%02x\n", 0xFF & dataL);
-    ioctl(fd, PPWDATA, &dataH);
-    ioctl(fd, PPWDATA, &dataL);
+	int fd;
+	fprintf(stderr, "Opening device %s\n", device);
+	if ( (fd=port_open(device)) == -1 ){
+		goto socket_cleanup; /* error already shown */
+	}
 
-    /* wait until a client connects */
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(sock, &fds);
-    if ( select(sock+1, &fds, NULL, NULL, NULL) == -1 ){
-      if ( errno != EINTR ){
-	perror("select");
-      }
-      continue;
-    }
+	/* all pins low */
+	char dataH = 0x00;
+	char dataL = 0x00;
 
-    struct sockaddr_un peer;
-    socklen_t len = sizeof(struct sockaddr_un);
-    int client = accept(sock, (struct sockaddr*)&peer, &len);
+	while ( running ){
+		printf("current output: 0x%02x\n", 0xFF & dataL);
+		ioctl(fd, PPWDATA, &dataH);
+		ioctl(fd, PPWDATA, &dataL);
 
-    if ( client == -1 ){
-      perror("accept error");
-      running = 0;
-      continue;
-    }
+		/* wait until a client connects */
+		fd_set fds;
+		FD_ZERO(&fds);
+		FD_SET(sock, &fds);
+		if ( select(sock+1, &fds, NULL, NULL, NULL) == -1 ){
+			if ( errno != EINTR ){
+				perror("select");
+			}
+			continue;
+		}
 
-    char buffer[128] = {0,};
-    ssize_t bytes = recv(client, buffer, sizeof(buffer), 0);
-    if ( bytes < 0 ){
-      fprintf(stderr, "failed to read client data: %s\n", strerror(errno));
-      shutdown(client, SHUT_RDWR);
-      continue;
-    }
+		struct sockaddr_un peer;
+		socklen_t len = sizeof(struct sockaddr_un);
+		int client = accept(sock, (struct sockaddr*)&peer, &len);
 
-    if ( buffer[bytes-1] == '\n' ){ /* remove trailing newline */
-      buffer[--bytes] = 0;
-    }
+		if ( client == -1 ){
+			perror("accept error");
+			running = 0;
+			continue;
+		}
 
-    fprintf(stderr, "client data: %*s\n", (int)bytes, buffer);
-    
-    char* cmd = strtok(buffer, " ");
-    if ( !cmd ){
-      cmd = buffer;
-    }
-    cmd = strdup(cmd);
+		char buffer[128] = {0,};
+		ssize_t bytes = recv(client, buffer, sizeof(buffer), 0);
+		if ( bytes < 0 ){
+			fprintf(stderr, "failed to read client data: %s\n", strerror(errno));
+			shutdown(client, SHUT_RDWR);
+			continue;
+		}
 
-    if ( strcmp(cmd, "set") == 0 ){
-      char* pin_str = strtok(NULL, " ");
-      char* action = strtok(NULL, " ");
-      int pin = 1 << (pin_str[1] - '0');
-      
-      if ( strcmp(action, "hi") == 0 ){
-	dataL |= pin;
-      } else if ( strcmp(action, "low") == 0 ){
-	dataL &= ~pin;
-      } else if ( strcmp(action, "toggle") == 0 ){
-	dataL ^= pin;
-      }
-    } else if ( strcmp(cmd, "strobe") == 0 ){
-      char* pin_str = strtok(NULL, " ");
-      char* time_str = strtok(NULL, " ");
-      int pin = 1 << (pin_str[1] - '0');
-      int time = atoi(time_str) * 1000; /* to µs */
+		if ( buffer[bytes-1] == '\n' ){ /* remove trailing newline */
+			buffer[--bytes] = 0;
+		}
 
-      dataL |= pin;
-      ioctl(fd, PPWDATA, &dataH);
-      ioctl(fd, PPWDATA, &dataL);
-      usleep(time);
-      dataL &= ~pin;
-    } else {
-      fprintf(stderr, "unknown command: %s\n", cmd);
-      bytes = snprintf(buffer, 128, "0;unknown command: %s", cmd);
-      send(client, buffer, (int)bytes, MSG_NOSIGNAL);
-    }
+		fprintf(stderr, "client data: %*s\n", (int)bytes, buffer);
 
-    free(cmd);
+		char* cmd = strtok(buffer, " ");
+		if ( !cmd ){
+			cmd = buffer;
+		}
+		cmd = strdup(cmd);
 
-    shutdown(client, SHUT_RDWR);
-  }
+		if ( strcmp(cmd, "set") == 0 ){
+			char* pin_str = strtok(NULL, " ");
+			char* action = strtok(NULL, " ");
+			int pin = 1 << (pin_str[1] - '0');
 
-  fprintf(stderr, "Shutting down\n");
-  exit_code = 0;
+			if ( strcmp(action, "hi") == 0 ){
+				dataL |= pin;
+			} else if ( strcmp(action, "low") == 0 ){
+				dataL &= ~pin;
+			} else if ( strcmp(action, "toggle") == 0 ){
+				dataL ^= pin;
+			}
+		} else if ( strcmp(cmd, "strobe") == 0 ){
+			char* pin_str = strtok(NULL, " ");
+			char* time_str = strtok(NULL, " ");
+			int pin = 1 << (pin_str[1] - '0');
+			int time = atoi(time_str) * 1000; /* to µs */
 
-  // Release and close the parallel port
-  ioctl(fd, PPRELEASE);
-  close(fd);
+			dataL |= pin;
+			ioctl(fd, PPWDATA, &dataH);
+			ioctl(fd, PPWDATA, &dataL);
+			usleep(time);
+			dataL &= ~pin;
+		} else {
+			fprintf(stderr, "unknown command: %s\n", cmd);
+			bytes = snprintf(buffer, 128, "0;unknown command: %s", cmd);
+			send(client, buffer, (int)bytes, MSG_NOSIGNAL);
+		}
 
- socket_cleanup:
-  unlink(sock_path);
+		free(cmd);
 
-  return exit_code;
+		shutdown(client, SHUT_RDWR);
+	}
+
+	fprintf(stderr, "Shutting down\n");
+	exit_code = 0;
+
+	// Release and close the parallel port
+	ioctl(fd, PPRELEASE);
+	close(fd);
+
+  socket_cleanup:
+	unlink(sock_path);
+
+	return exit_code;
 }
